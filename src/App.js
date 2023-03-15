312133ca3f1801bcac45bf266c779dd052c6be31
chris martinex <uschrismartinez@gmail.com>
2023-03-14 21:38:43 -0700

    peer review

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Generator from './Components/GenaratorComponent';
 function App() {
   return (
     <div className="App">
-     
-      
-      <Generator/>
-    
+         <Generator/>
     </div>
     
   );
 }
 
 export default App;
+
+// Peer Review 03/14/23
+// Jessie Lamzon
+// Peer Review: Advice Generator
+// I like how you used useeffect to call the fetch when page loads already, also having the divider set in a fixed position too.  I would say the font sizes need updating and hover effect is also missing. Other than that the fetch and everything works great.
